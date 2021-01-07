c05863cf0f04eaec0963b998b56f85e5a83747c9
Anton van Hensbergen <anton.van.hensbergen@gmail.com>
2021-01-07 23:56:02 +0100

    versie 12

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,24 +1,25 @@
 <?php
 
 
-namespace App\Controller;
+namespace App\Controller {
 
 
-use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+    use App\Entity\User;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
-{
-    /**
-     * @Route("/", name="visitor_home")
-     */
-    public function defaultAction():Response{
-        $principal=$this->getDoctrine()->getRepository(User::class)->findPrincipal();
-        $teachers = $this->getDoctrine()->getRepository(User::class)->findTeachers();
-        $administrators = $this->getDoctrine()->getRepository(User::class)->findAdministrators();
-        return $this->render('visitor/default.html.twig',
-            [  'principal'=>$principal, 'teachers'=>$teachers,'administrators'=>$administrators]);
+    class DefaultController extends AbstractController
+    {
+        /**
+         * @Route("/", name="visitor_home")
+         */
+        public function defaultAction():Response{
+            $principal=$this->getDoctrine()->getRepository(User::class)->findPrincipal();
+            $teachers = $this->getDoctrine()->getRepository(User::class)->findTeachers();
+            $administrators = $this->getDoctrine()->getRepository(User::class)->findAdministrators();
+            return $this->render('visitor/default.html.twig',
+                [  'principal'=>$principal, 'teachers'=>$teachers,'administrators'=>$administrators]);
+        }
     }
 }
