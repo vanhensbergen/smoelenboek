8c87a1ff4eefe2de4c505c5ef51db5475babb98e
Anton van Hensbergen <anton.van.hensbergen@gmail.com>
2021-01-13 23:49:23 +0100

    versie 25 minor view code update

--- src/Entity/Schoolclass.php
+++ src/Entity/Schoolclass.php
@@ -1,123 +1,124 @@
 <?php
 
-namespace App\Entity;
+namespace App\Entity {
 
-use App\Repository\SchoolclassRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * @ORM\Entity(repositoryClass=SchoolclassRepository::class)
- * @ORM\Table(name="schoolclasses")
- */
-class Schoolclass
-{
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private ?int $id;
+    use App\Repository\SchoolclassRepository;
+    use Doctrine\Common\Collections\ArrayCollection;
+    use Doctrine\Common\Collections\Collection;
+    use Doctrine\ORM\Mapping as ORM;
 
     /**
-     * @ORM\Column(type="string", length=10, unique=true)
+     * @ORM\Entity(repositoryClass=SchoolclassRepository::class)
+     * @ORM\Table(name="schoolclasses")
      */
-    private ?string $name;
-
-    /**
-     * @ORM\Column(type="text")
-     */
-    private ?string $description;
-
-    /**
-     * one schoolclass has one mentor
-     * @ORM\OneToOne(targetEntity=User::class, inversedBy="mentorclass")
-     */
-    private ?User $mentor;
-
-    /**
-     * @ORM\OneToMany(targetEntity=User::class, mappedBy="schoolclass")
-     */
-    private $users;
-
-    public function __construct()
+    class Schoolclass
     {
-        $this->users = new ArrayCollection();
-    }
+        /**
+         * @ORM\Id
+         * @ORM\GeneratedValue
+         * @ORM\Column(type="integer")
+         */
+        private ?int $id;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+        /**
+         * @ORM\Column(type="string", length=10, unique=true)
+         */
+        private ?string $name;
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
+        /**
+         * @ORM\Column(type="text")
+         */
+        private ?string $description;
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
+        /**
+         * one schoolclass has one mentor
+         * @ORM\OneToOne(targetEntity=User::class, inversedBy="mentorclass")
+         */
+        private ?User $mentor;
 
-        return $this;
-    }
+        /**
+         * @ORM\OneToMany(targetEntity=User::class, mappedBy="schoolclass")
+         */
+        private $users;
 
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getMentor(): ?User
-    {
-        return $this->mentor;
-    }
-
-    public function setMentor(?User $mentor): self
-    {
-        $this->mentor = $mentor;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|User[]
-     */
-    public function getUsers(): Collection
-    {
-        $array = $this->users->toArray();
-        usort($array,fn($a,$b)=>$a->getLastname()>$b->getLastname()?1:-1
-        );
-        $this->users = new ArrayCollection($array);
-        return $this->users;
-    }
-
-    public function addUser(User $user): self
-    {
-        if (!$this->users->contains($user)) {
-            $this->users[] = $user;
-            $user->setSchoolclass($this);
+        public function __construct()
+        {
+            $this->users = new ArrayCollection();
         }
 
-        return $this;
-    }
+        public function getId(): ?int
+        {
+            return $this->id;
+        }
 
-    public function removeUser(User $user): self
-    {
-        if ($this->users->removeElement($user)) {
-            // set the owning side to null (unless already changed)
-            if ($user->getSchoolclass() === $this) {
-                $user->setSchoolclass(null);
+        public function getName(): ?string
+        {
+            return $this->name;
+        }
+
+        public function setName(string $name): self
+        {
+            $this->name = $name;
+
+            return $this;
+        }
+
+        public function getDescription(): ?string
+        {
+            return $this->description;
+        }
+
+        public function setDescription(string $description): self
+        {
+            $this->description = $description;
+
+            return $this;
+        }
+
+        public function getMentor(): ?User
+        {
+            return $this->mentor;
+        }
+
+        public function setMentor(?User $mentor): self
+        {
+            $this->mentor = $mentor;
+
+            return $this;
+        }
+
+        /**
+         * @return Collection|User[]
+         */
+        public function getUsers(): Collection
+        {
+            $array = $this->users->toArray();
+            usort($array,fn($a,$b)=>$a->getLastname()>$b->getLastname()?1:-1
+            );
+            $this->users = new ArrayCollection($array);
+            return $this->users;
+        }
+
+        public function addUser(User $user): self
+        {
+            if (!$this->users->contains($user)) {
+                $this->users[] = $user;
+                $user->setSchoolclass($this);
             }
+
+            return $this;
         }
 
-        return $this;
+        public function removeUser(User $user): self
+        {
+            if ($this->users->removeElement($user)) {
+                // set the owning side to null (unless already changed)
+                if ($user->getSchoolclass() === $this) {
+                    $user->setSchoolclass(null);
+                }
+            }
+
+            return $this;
+        }
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,329 +1,330 @@
 <?php
 
-namespace App\Entity;
+namespace App\Entity {
 
-use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
-
-/**
- * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="users")
- */
-class User implements UserInterface
-{
-    private const IMAGE_PATH = 'img/';
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+    use App\Repository\UserRepository;
+    use Doctrine\Common\Collections\ArrayCollection;
+    use Doctrine\Common\Collections\Collection;
+    use Doctrine\ORM\Mapping as ORM;
+    use Symfony\Component\Security\Core\User\UserInterface;
 
     /**
-     * @ORM\Column(type="string", length=180, unique=true)
+     * @ORM\Entity(repositoryClass=UserRepository::class)
+     * @ORM\Table(name="users")
      */
-    private $email;
-
-    /**
-     * @ORM\Column(type="json")
-     */
-    private array $roles = [];
-
-    /**
-     * @var string The hashed password
-     * @ORM\Column(type="string")
-     */
-    private $password;
-
-    /**
-     * @ORM\Column(type="string", length=50, nullable=false)
-     */
-    private  $firstname;
-
-    /**
-     * @ORM\Column(type="string", length=20, nullable=true)
-     */
-    private $prefix;
-
-    /**
-     * @ORM\Column(type="string", length=50)
-     */
-    private $lastname;
-
-    /**
-     * @ORM\Column(type="string", length=50, nullable=true)
-     */
-    private $photo;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Schoolclass::class, inversedBy="users")
-     */
-    private $schoolclass;
-    /**
-     * one mentor has one class
-     * @ORM\OneToOne(targetEntity=Schoolclass::class, mappedBy="mentor")
-     */
-    private $mentorclass;
-
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $motto;
-
-    /**
-     * @ORM\OneToMany(targetEntity=StudentRemark::class, mappedBy="author", orphanRemoval=true)
-     * represents the remarks as a collection made by the author
-     */
-    private $remarksAsAuthor;
-
-    /**
-     * @ORM\OneToMany(targetEntity=StudentRemark::class, mappedBy="student", orphanRemoval=true)
-     * represents the remarks as a collection made for the student
-     */
-    private $remarksForStudent;
-
-    public function __construct()
+    class User implements UserInterface
     {
-        if(in_array("ROLE_STUDENT", $this->roles)) {
-            $this->remarksForStudent = new ArrayCollection();
-        }
-        else {
-            $this->remarksAsAuthor = new ArrayCollection();
-        }
-    }
+        private const IMAGE_PATH = 'img/';
+        /**
+         * @ORM\Id
+         * @ORM\GeneratedValue
+         * @ORM\Column(type="integer")
+         */
+        private $id;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+        /**
+         * @ORM\Column(type="string", length=180, unique=true)
+         */
+        private $email;
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
+        /**
+         * @ORM\Column(type="json")
+         */
+        private array $roles = [];
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
+        /**
+         * @var string The hashed password
+         * @ORM\Column(type="string")
+         */
+        private $password;
 
-        return $this;
-    }
+        /**
+         * @ORM\Column(type="string", length=50, nullable=false)
+         */
+        private  $firstname;
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUsername(): string
-    {
-        return (string) $this->email;
-    }
+        /**
+         * @ORM\Column(type="string", length=20, nullable=true)
+         */
+        private $prefix;
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        /**
+         * @ORM\Column(type="string", length=50)
+         */
+        private $lastname;
 
-        return array_unique($roles);
-    }
+        /**
+         * @ORM\Column(type="string", length=50, nullable=true)
+         */
+        private $photo;
 
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
+        /**
+         * @ORM\ManyToOne(targetEntity=Schoolclass::class, inversedBy="users")
+         */
+        private $schoolclass;
+        /**
+         * one mentor has one class
+         * @ORM\OneToOne(targetEntity=Schoolclass::class, mappedBy="mentor")
+         */
+        private $mentorclass;
 
-        return $this;
-    }
+        /**
+         * @ORM\Column(type="text", nullable=true)
+         */
+        private $motto;
 
-    /**
-     * @see UserInterface
-     */
-    public function getPassword(): string
-    {
-        return (string) $this->password;
-    }
+        /**
+         * @ORM\OneToMany(targetEntity=StudentRemark::class, mappedBy="author", orphanRemoval=true)
+         * represents the remarks as a collection made by the author
+         */
+        private $remarksAsAuthor;
 
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
+        /**
+         * @ORM\OneToMany(targetEntity=StudentRemark::class, mappedBy="student", orphanRemoval=true)
+         * represents the remarks as a collection made for the student
+         */
+        private $remarksForStudent;
 
-        return $this;
-    }
-
-    /**
-     * @see UserInterface
-     */
-    public function getSalt()
-    {
-        // not needed when using the "bcrypt" algorithm in security.yaml
-    }
-
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials()
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
-
-    public function getFirstname(): ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(string $firstname): self
-    {
-        $this->firstname = $firstname;
-
-        return $this;
-    }
-
-    public function getPrefix(): ?string
-    {
-        return $this->prefix;
-    }
-
-    public function setPrefix(?string $prefix): self
-    {
-        $this->prefix = $prefix;
-
-        return $this;
-    }
-
-    public function getLastname(): ?string
-    {
-        return $this->lastname;
-    }
-
-    public function setLastname(string $lastname): self
-    {
-        $this->lastname = $lastname;
-
-        return $this;
-    }
-
-    public function getPhoto(): ?string
-    {
-        $value = $this->photo??"nn.jpg";
-        return self::IMAGE_PATH.$value;
-    }
-
-    public function getPhotoFileName():?string{
-        return $this->photo;
-    }
-
-    public function setPhoto(?string $photo): self
-    {
-        $this->photo = $photo;
-
-        return $this;
-    }
-
-    public function getSchoolclass(): ?Schoolclass
-    {
-        return $this->schoolclass;
-    }
-
-    public function setSchoolclass(?Schoolclass $schoolclass): self
-    {
-        $this->schoolclass = $schoolclass;
-
-        return $this;
-    }
-
-    public function getMentorclass(): ?Schoolclass
-    {
-        return $this->mentorclass;
-    }
-
-    public function setMentorclass(?Schoolclass $mentorclass): self
-    {
-        $this->mentorclass = $mentorclass;
-
-        return $this;
-    }
-    public function getFullName():string
-    {
-        $prefix = $this->prefix?:"";
-        return "$this->firstname $prefix $this->lastname";
-    }
-
-    public function getMotto(): ?string
-    {
-        return $this->motto;
-    }
-
-    public function setMotto(?string $motto): self
-    {
-        $this->motto = $motto;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|StudentRemark[]
-     */
-    public function getRemarksAsAuthor(): Collection
-    {
-        return $this->remarksAsAuthor;
-    }
-
-    public function addRemarkAsAuthor(StudentRemark $remarkByMe): self
-    {
-        if (!$this->remarksAsAuthor->contains($remarkByMe)) {
-            $this->remarksAsAuthor[] = $remarkByMe;
-            $remarkByMe->setAuthor($this);
-        }
-
-        return $this;
-    }
-
-    public function removeRemarkAsAuthor(StudentRemark $remarkByMe): self
-    {
-        if ($this->remarksAsAuthor->removeElement($remarkByMe)) {
-            // set the owning side to null (unless already changed)
-            if ($remarkByMe->getAuthor() === $this) {
-                $remarkByMe->setAuthor(null);
+        public function __construct()
+        {
+            if(in_array("ROLE_STUDENT", $this->roles)) {
+                $this->remarksForStudent = new ArrayCollection();
+            }
+            else {
+                $this->remarksAsAuthor = new ArrayCollection();
             }
         }
 
-        return $this;
-    }
-
-    /**
-     * @return Collection|StudentRemark[]
-     */
-    public function getRemarksForStudent(): Collection
-    {
-        return $this->remarksForStudent;
-    }
-
-    public function addRemarkForStudent(StudentRemark $remarkForMe): self
-    {
-        if (!$this->remarksForStudent->contains($remarkForMe)) {
-            $this->remarksForStudent[] = $remarkForMe;
-            $remarkForMe->setStudent($this);
+        public function getId(): ?int
+        {
+            return $this->id;
         }
 
-        return $this;
-    }
+        public function getEmail(): ?string
+        {
+            return $this->email;
+        }
 
-    public function removeRemarkForStudent(StudentRemark $remarkForMe): self
-    {
-        if ($this->remarksForStudent->removeElement($remarkForMe)) {
-            // set the owning side to null (unless already changed)
-            if ($remarkForMe->getAuthor() === $this) {
-                $remarkForMe->setAuthor(null);
+        public function setEmail(string $email): self
+        {
+            $this->email = $email;
+
+            return $this;
+        }
+
+        /**
+         * A visual identifier that represents this user.
+         *
+         * @see UserInterface
+         */
+        public function getUsername(): string
+        {
+            return (string) $this->email;
+        }
+
+        /**
+         * @see UserInterface
+         */
+        public function getRoles(): array
+        {
+            $roles = $this->roles;
+            // guarantee every user at least has ROLE_USER
+            $roles[] = 'ROLE_USER';
+
+            return array_unique($roles);
+        }
+
+        public function setRoles(array $roles): self
+        {
+            $this->roles = $roles;
+
+            return $this;
+        }
+
+        /**
+         * @see UserInterface
+         */
+        public function getPassword(): string
+        {
+            return (string) $this->password;
+        }
+
+        public function setPassword(string $password): self
+        {
+            $this->password = $password;
+
+            return $this;
+        }
+
+        /**
+         * @see UserInterface
+         */
+        public function getSalt()
+        {
+            // not needed when using the "bcrypt" algorithm in security.yaml
+        }
+
+        /**
+         * @see UserInterface
+         */
+        public function eraseCredentials()
+        {
+            // If you store any temporary, sensitive data on the user, clear it here
+            // $this->plainPassword = null;
+        }
+
+        public function getFirstname(): ?string
+        {
+            return $this->firstname;
+        }
+
+        public function setFirstname(string $firstname): self
+        {
+            $this->firstname = $firstname;
+
+            return $this;
+        }
+
+        public function getPrefix(): ?string
+        {
+            return $this->prefix;
+        }
+
+        public function setPrefix(?string $prefix): self
+        {
+            $this->prefix = $prefix;
+
+            return $this;
+        }
+
+        public function getLastname(): ?string
+        {
+            return $this->lastname;
+        }
+
+        public function setLastname(string $lastname): self
+        {
+            $this->lastname = $lastname;
+
+            return $this;
+        }
+
+        public function getPhoto(): ?string
+        {
+            $value = $this->photo??"nn.jpg";
+            return self::IMAGE_PATH.$value;
+        }
+
+        public function getPhotoFileName():?string{
+            return $this->photo;
+        }
+
+        public function setPhoto(?string $photo): self
+        {
+            $this->photo = $photo;
+
+            return $this;
+        }
+
+        public function getSchoolclass(): ?Schoolclass
+        {
+            return $this->schoolclass;
+        }
+
+        public function setSchoolclass(?Schoolclass $schoolclass): self
+        {
+            $this->schoolclass = $schoolclass;
+
+            return $this;
+        }
+
+        public function getMentorclass(): ?Schoolclass
+        {
+            return $this->mentorclass;
+        }
+
+        public function setMentorclass(?Schoolclass $mentorclass): self
+        {
+            $this->mentorclass = $mentorclass;
+
+            return $this;
+        }
+        public function getFullName():string
+        {
+            $prefix = $this->prefix?:"";
+            return "$this->firstname $prefix $this->lastname";
+        }
+
+        public function getMotto(): ?string
+        {
+            return $this->motto;
+        }
+
+        public function setMotto(?string $motto): self
+        {
+            $this->motto = $motto;
+
+            return $this;
+        }
+
+        /**
+         * @return Collection|StudentRemark[]
+         */
+        public function getRemarksAsAuthor(): Collection
+        {
+            return $this->remarksAsAuthor;
+        }
+
+        public function addRemarkAsAuthor(StudentRemark $remarkByMe): self
+        {
+            if (!$this->remarksAsAuthor->contains($remarkByMe)) {
+                $this->remarksAsAuthor[] = $remarkByMe;
+                $remarkByMe->setAuthor($this);
             }
+
+            return $this;
         }
 
-        return $this;
+        public function removeRemarkAsAuthor(StudentRemark $remarkByMe): self
+        {
+            if ($this->remarksAsAuthor->removeElement($remarkByMe)) {
+                // set the owning side to null (unless already changed)
+                if ($remarkByMe->getAuthor() === $this) {
+                    $remarkByMe->setAuthor(null);
+                }
+            }
+
+            return $this;
+        }
+
+        /**
+         * @return Collection|StudentRemark[]
+         */
+        public function getRemarksForStudent(): Collection
+        {
+            return $this->remarksForStudent;
+        }
+
+        public function addRemarkForStudent(StudentRemark $remarkForMe): self
+        {
+            if (!$this->remarksForStudent->contains($remarkForMe)) {
+                $this->remarksForStudent[] = $remarkForMe;
+                $remarkForMe->setStudent($this);
+            }
+
+            return $this;
+        }
+
+        public function removeRemarkForStudent(StudentRemark $remarkForMe): self
+        {
+            if ($this->remarksForStudent->removeElement($remarkForMe)) {
+                // set the owning side to null (unless already changed)
+                if ($remarkForMe->getAuthor() === $this) {
+                    $remarkForMe->setAuthor(null);
+                }
+            }
+
+            return $this;
+        }
     }
 }
